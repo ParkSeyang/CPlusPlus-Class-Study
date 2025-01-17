ad8408464d3b3429301726f845afd0846af9f952
ParkSeyang <ghlwkd14@naver.com>
2025-01-17 19:45:38 +0900

    (2025.01.17) 캡슐화

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,85 +2,77 @@
 
 using namespace std;
 
-class Unit
+class Terran
 {
+protected:
 
-private:
-	int health = 0;
-
-	int* kill;
-
+	int health;
+	int defence;
 public:
-
-	Unit(int health)
+	Terran()
 	{
-#pragma region This 포인터
-		// 자기 자신을 가리키는 포인터입니다.
-
-#pragma endregion
-
-		this->health = health;
-
-		kill = new int;
-		*kill = 0;
-	}
-	
-	Unit(const Unit& clone)
-	{
-		health = clone.health;
-
-		kill = clone.kill;
-	}
-
-	const int & Health()
-	{
-		return health;
-	}
-	~Unit()
-	{
-		delete kill;
+		cout << "Created Terran" << endl;
 	}
 };
 
-//생성자 함수
-// Unit::Unit()
-// {
-// }
-// 소멸자 함수
-// Unit::~Unit()
-// {
-// }
+class Marine :public Terran
+{
+private:
+	int attack;
+public:
+	Marine()
+	{
+		cout << "Created Marine" << endl;
+	}
+};
+
+class Firebat :public Terran
+{
+private:
+	int attack;
+public:
+	Firebat()
+	{
+		cout << "Created Firebat" << endl;
+	}
+}; 
+
+class Ghost :public Terran
+{
+private:
+	int attack;
+public:
+	Ghost()
+	{
+		cout << "Created Ghost" << endl;
+	}
+};
 
 int main()
 {
-#pragma region 복사 생성자
-	// 같은 객체를 복사하여 생성시킬 때 호출되는
-	// 생성자입니다.
-
-	// Unit marine1(40);
-	// Unit marine2(marine1);
-
-	// marine.Health() = 5;
-
-	// cout << marine1.Health() << endl;
-	// cout << marine2.Health() << endl;
-
-	// 복사 생성자를 정의하지 않고, 객체를 복사하게 되면
-	// 기본 복사 생성자가 호출되기 때문에 얕은 복사로 객체가 복사 됩니다.
+#pragma region 캡슐화
+	// 클래스 안에 서로 연관되어 잇는 속성과 기능들을 하나의 캡슐로
+	// 만들어 데이터를 외부로부터 보호 하며, 사용자에게 인터페이스를
+	// 제공하여 클래스의 기능을 사용하는 것입니다. 
+	// (사용자로부터 내부에 있는 데이터를 사용하지 않고,
+	// 인터페이스를 사용할 수 있도록 설정하는 것입니다.)
 #pragma endregion
 
-#pragma region 얕은 복사
-	// 객체를 복사할 때 주소값을 복사하여 같은
-	// 메모리 공간을 가리키게 하는 것입니다.
-	
-	
-	Unit firebat1(50);
+#pragma region 상속
+	// 상위 클래스의 속성을 하위 클래스가 사용할 수 있도록
+	// 설정 해주는 기능입니다.
 
-	Unit firebat2(firebat1);
+	Terran terran;
 
-	// 얕은 복사의 경우 같은 객체가 서로 같은 메모리
-	// 공간을 참조하고 있기 때문에 하나의 객체로 값을
-	// 변경하게 되면 서로 참조된 객체도 함께 영향을 받습니다.
+	Marine marine;
+
+	Firebat firebat;
+	
+	// Ghost ghost;
+
+	// 클래스의 상속 관계에서 상위 클래스는 하위 클래스의
+	// 속성을 사용할 수 없으며, 하위 클래스는 상위 클래스의
+	// 메모리가 포함된 상태로 메모리의 크기가 결정됩니다.
 #pragma endregion
 
 	return 0;
