6808117aae6e6d65b0ccbca88c1750808de761a0
ParkSeyang <ghlwkd14@naver.com>
2025-02-10 19:44:42 +0900

    (2025.02.10) 연관 컨테이너

---
+++ Class 14th (Container Adapter)/Source.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+#pragma region Stack
+	// std::stack<int> stack;
+	// 
+	// stack.push(10);
+	// stack.push(20);
+	// stack.push(30);
+	// 
+	// cout << "Stack 의 Size : " << stack.size() << endl;
+	// 
+	// while (stack.empty() == false)
+	// {
+	// 	cout << "data :" << stack.top() << endl;
+	// 
+	// 	stack.pop();
+	// }
+
+	// 데이터 가 쌓이는방식은 먼저들어간 데이터는 맨나중에 나온다.
+
+#pragma endregion
+
+#pragma region Queue
+	// std::queue<int> queue;
+	// 
+	// queue.push(10);
+	// queue.push(20);
+	// queue.push(30);
+	// queue.push(40);
+	// queue.push(50);
+	// 
+	// cout << "Queue Size : " << queue.size() << endl;
+	// 
+	// cout << "queue push data : " << queue.front() << endl;
+	// queue.pop();
+	// cout << "queue push data : " << queue.front() << endl;
+	// queue.pop();
+	// cout << "queue push data : " << queue.front() << endl;
+	// queue.pop();
+	// cout << "queue push data : " << queue.front() << endl;
+	// queue.pop();
+	// cout << "queue push data : " << queue.front() << endl;
+	// queue.pop();
+
+	// while (queue.empty() == false)
+	// {
+	// 	cout << "queue push data : " << queue.front() << endl;
+	// 	
+	// 	queue.pop();
+	// }
+
+
+
+
+#pragma endregion
+
+
+	return 0;
+}
+
--- Program/Source.cpp
+++ Program/Source.cpp
@@ -1,30 +1,10 @@
 ﻿#include <iostream>
-#include <stack>
 
 
 using namespace std;
 
 int main()
 {
-#pragma region Stack
-	std::stack<int> stack;
-	
-	stack.push(10);
-	stack.push(20);
-	stack.push(30);
-
-	cout << "Stack 의 Size : " << stack.size() << endl;
-
-	while (stack.empty() == false)
-	{
-		cout << "data :" << stack.top() << endl;
-
-		stack.pop();
-	}
-	
-	
-	
-#pragma endregion
 
 	return 0;
 }
